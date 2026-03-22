3c46ba78085abe2b4d5473c9205fa74840dbfce3
luanlucolli <luangstl@gmail.com>
2026-03-22 01:27:08 -0300

    remove unused area, imc, and teste scripts

--- area.py
+++
@@ -1,7 +0,0 @@
-base = int(input("base: "))
-altura = int(input("alutura: "))
-
-if base < 0 or altura < 0:
-    quit()
-
-print(base * altura)
--- teste.py
+++
@@ -1,9 +0,0 @@
-n = 30
-f = 3.14
-t = "15"
-b = True
-
-print(type(n), n)
-print(type(f))
-print(type(t))
-print(type(b))
