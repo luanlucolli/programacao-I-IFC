c0b52e17bc9ba37a980183f50383ce94acff947c
luanlucolli <luangstl@gmail.com>
2026-06-10 17:47:22 -0300

    feat(trabalho final): conclusão das etapas 1, 2 e 3:
    - criar arquivos principal.py e outros tres arquivos
    
    - criar menu com opções para usuario
    
    - criar funções de boas vindas e despedida

---
+++ trabalho-final/funcoes_gerais.py
@@ -0,0 +1,9 @@
+def inicio_prog():
+    print("===================")
+    print("  Bem vindo, Luan  ")
+    print("===================")
+    
+def fim_prog():
+    print("===================")
+    print("  Até logo, Luan   ")
+    print("===================")
---
+++ trabalho-final/principal.py
@@ -0,0 +1,51 @@
+import funcoes_gerais as fng
+import funcoes_especificas as fne
+
+'''2. (1 ponto) - Crie um menu que apresente as seguintes opções ao usuário:
+Escolha uma das opções:
+S ou 0 - Sair
+M ou 1 - Dias
+E ou 2 - Expressões Matemáticas
+F ou 3 - F0, F1, F2 ou F3
+A ou 4 - Somatório e Média
+P ou 5 - Par ou Ímpar
+X ou 6 - Operações Matemáticas'''
+def menu():
+    
+    op = ''
+    
+    while op != 0 and op != "S" and op != "s":
+        
+        print("Opções:")
+        print("S ou 0 - Sair")
+        print("M ou 1 - Dias")
+        print("E ou 2 - Expressões Matemáticas")
+        print("F ou 3 - F0, F1, F2 ou F3")
+        print("A ou 4 - Somatório e Média")
+        print("P ou 5 - Par ou Ìmpar")
+        print("X ou 6 - Operações Matemáticas")
+
+        op = input("Escolha uma das opções: ")
+        
+        match op:
+            case "S" | "s" | 0:
+                fng.fim_prog()
+            case "M" | "m" | 1:
+                print("dias")
+            case "E" | "e" | 2:
+                print("expressões matematicas")            
+            case "F" | "f" | 3:
+                print("f0,f1,f2,f3")
+            case "A" | "a" | 4:
+                print("somatorio ou media")
+            case "P" | "p" | 5:
+                print("par ou impar")   
+            case "X" | "x" | 6:
+                print("operacoes matematicas")   
+                
+# Função principal                 
+def main():
+    fng.inicio_prog()
+    menu()
+    
+main()
