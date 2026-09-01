b2e0fce0dadb885cc17b2d53996d7e97ed185755
luanlucolli <luangstl@gmail.com>
2026-09-01 19:37:44 -0300

    refactor(vetores-e-matrizes-25-08-2026): ajusta import da lib random no exercício 3

--- programacao-II/vetores-e-matrizes-25-08-2026/3.py
+++ programacao-II/vetores-e-matrizes-25-08-2026/3.py
@@ -1,6 +1,6 @@
 """3 - Faça um programa que gere o segiunte array: [0,0,0,0]. Substitua os valores zerados por números aleatórios. Imprima o array na tela. Depois apresente o somátório e a média do array de números aleatórios."""
 
-import random
+from random import randint
 
 numbers = [0, 0, 0, 0]
 soma = 0
@@ -8,9 +8,9 @@ media = 0
 numbersLen = len(numbers)
 
 for x in range(numbersLen):
-    numbers[x] = random.randint(0, 999)
+    numbers[x] = randint(0, 999)
     soma += numbers[x]
-    if x == numbersLen-1:
+    if x == numbersLen - 1:
         media = soma / numbersLen
 
 print(numbers)
